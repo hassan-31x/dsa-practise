b3c5aa103e98ecf344d1388ac6c94a86ecea92ae
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-01 10:40:59 +0500

    day-001.cpp - advance data types & comments

--- day-001.cpp
+++ day-001.cpp
@@ -16,5 +16,15 @@ int main() {
     int size = sizeof(a);
     cout << "size of a is: " << size << "bytes" <<endl;
     */
+    
+    /*
+    int a = 'a'; //returns ASCII value of 'a' i.e 97
+    char x = 98; //returns character for this ASCII
+    char y = 123456; //large value so it will assign the highest possible value(64) to y
+
+    unsigned int a = 876; //using unsigned int rather than int will not allow to store negative number but range will be increased
+
+    // int/int=int float/int=float double/int=double
+    */
 
 }
