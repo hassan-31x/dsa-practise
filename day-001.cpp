c440381c3fd80d8acbf8296758786dc34267d891
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-01 10:40:10 +0500

    day-001.cpp - basic data types

--- day-001.cpp
+++ day-001.cpp
@@ -4,4 +4,17 @@ using namespace std;
 
 int main() {
 
+    /*
+    cout<< "hello world" <<endl; / cout<< "hello wolrd\n";
+
+    int a = 32;
+    char b = 'x';
+    float x = 1.2;
+    double y = 1.23;
+    bool z = true (printing z will print 1)
+
+    int size = sizeof(a);
+    cout << "size of a is: " << size << "bytes" <<endl;
+    */
+
 }
