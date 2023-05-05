996b15c2c72e1b0eea6168a179f287b22ca852ed
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-06 00:25:23 +0500

    pattern questions - advance

--- day-004.cpp
+++ day-004.cpp
@@ -258,5 +258,212 @@ int main() {
         row = row + 1;
     }
     */
+    
+    //Pattern Question 16 (Alphabets goes until input n with row number of columns for n rows - triangle)
+    /*
+    int n;
+    cin>> n;
+
+    int row = 1;
+
+    while (row <= n) {
+        int column = row;
+
+        while (column > 0) {
+            char character = 'A' + (n - column + 1) - 1;
+            cout<< character << " ";
+            column = column - 1;
+        }
+        cout<<endl;
+
+        row = row + 1;
+    }
+    */
+
+    //Number of rows are same in both but in first it goes (0 - num) and in second it goes (num - 0) [forward and backward]
+    /*
+    while (row <= n) {
+        int column = 1;
+
+        while (column <= row) {
+            column = column + 1;
+        }
+        cout<<endl;
+        row = row + 1;
+    }
+    while (row <= n) {
+        int column = row;
+
+        while (column > 0) {
+            column = column - 1;
+        }
+        cout<<endl;
+        row = row + 1;
+    }
+    */
+
+    //Pattern Question 17
+    /*
+    int n;
+    cin>> n;
+
+    int row = 1;
+
+    while (row <= n) {
+        int column = 1;
+        int space = n - row + 1;
+        
+        while (space > 1) { //OR while (space) { 
+            cout<< " "; 
+            space = space - 1;
+        }
+        while (column <= row) {
+            cout<< "*"; 
+            column = column + 1;
+        }
+        cout<<endl;
+
+        row = row + 1;
+    }
+    */
+
+    //Pattern Question 18 (same as above but inverted triangle - triangle)
+    /*
+    int n;
+    cin>> n;
+
+    int row = n;
+
+    while (row >= 1) {
+        int column = 1;
+
+        while (column <= row) {
+            cout<< "*"; 
+            column = column + 1;
+        }
+        cout<<endl;
+
+        row = row - 1;
+    }
+    */
+    //same can be done by printing using formula (n-row + 1) without starting rows from end
+
+    //Pattern Question 19 (combine question 17 and 18)
+    /*
+    int n;
+    cin>> n;
+
+    int row = n;
+
+    while (row >= 1) {
+        int column = 1;
+        int space = n - row + 1;
+        
+        while (space > 1) { //OR while (space) { 
+            cout<< " "; 
+            space = space - 1;
+        }
+        while (column <= row) {
+            cout<< "*"; 
+            column = column + 1;
+        }
+        cout<<endl;
+
+        row = row - 1;
+    }
+    */
+
+    //Pattern Question 20 (same as above but replace star with row number in asc order)
+    /*
+    int n;
+    cin>> n;
+
+    int row = n;
+
+    while (row >= 1) {
+        int column = 1;
+        int space = n - row + 1;
+        
+        while (space > 1) { //OR while (space) { 
+            cout<< " "; 
+            space = space - 1;
+        }
+        while (column <= row) {
+            cout<< n-row + 1; 
+            column = column + 1;
+        }
+        cout<<endl;
+
+        row = row - 1;
+    }
+    */
+
+    //Patter Question 21 (Fibonacci Tree - triangle)
+    /*int n;
+    cin>> n;
+
+    int row = 1;
+    while (row <= n) {
+
+        //print spaces (first triangle)
+        int space = n - row;
+        while (space) {
+            cout<< " ";
+            space = space - 1;
+        }
+
+        //print increasing numbers (second triangle)
+        int column = 1;
+        while (column <= row) {
+            cout<< column;
+            column = column + 1;
+        }
+
+        //print decreasing numbers except first row (third triangle)
+        int start = row - 1;
+        while (start) {
+            cout<< start;
+            start = start - 1;
+        }
+
+        cout<<endl;
+        row = row + 1;
+    }
+    */
+
+    //Pattern Question 22
+    /*
+    int n;
+    cin>> n;
+    
+    int row = 1;
+    int stars = 0;
+    while (row <= n) {
+
+        //first triangle (decreasing numbers)
+        int column = 1;
+        while(column <= n-row+1) {
+            cout<< (column++);
+        }
+
+        //second triangle (stars)
+        int star = stars;
+        while (star) {
+            cout<< "*";
+            star = star - 1;
+        }
+        stars = stars + 2;
+
+        //third triangle
+        column = n-row + 1;
+        while (column) {
+            cout<< (column--);
+        }
+
+
+        cout<<endl;
+        row = row + 1;
+    }
+    */
   
 }
