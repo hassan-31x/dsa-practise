7d8c82022f6e5fb7523be05b76608a70ed145a53
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-05 23:45:30 +0500

    conditionals & loops

--- day-003.cpp
+++ day-003.cpp
@@ -1,1 +1,43 @@
+//INPUTS & WHILE LOOP
+#include<iostream>
+using namespace std;
 
+int main() {
+    
+    //Input
+    /*
+    int n;
+    cin>> n;
+    cout<< n <<endl;
+    */
+
+    //Conditional Statement
+    /*
+    int a;
+    cin>> a;
+
+    if (a>0) {
+        cout<< "a is positive" <<endl;
+    }
+    else {
+        cout<< "a is negative" <<endl;
+    }
+    */
+
+    //While Loop
+    /*
+    int a,b;
+    cout<< "Enter the value of a and b" <<endl;
+    cin>> a >> b;
+    cout<< "value of A and B is: " << a << " and "<< b <<endl;
+    */
+
+    /*
+    int n;
+    while (n <= 10) {
+        cout << "value of n is: " << n;
+        n = n+!
+    }
+    */
+  
+}
