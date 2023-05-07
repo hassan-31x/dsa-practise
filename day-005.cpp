375ec937aa06ea0eec54d55088d7757e4493eff7
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-07 16:32:13 +0500

    increment/decrement

--- day-005.cpp
+++ day-005.cpp
@@ -19,5 +19,25 @@ int main() {
     cout<< (17>>2) <<endl;
     cout<< (19<<1) <<endl;
     cout<< (21<<2) <<endl;
+    
+    // Pre & Post increment/decrement
+    int i = 7;
+
+    cout<< (++i) <<endl;
+    // 8
+    cout<< (i++) <<endl;
+    // 8 , i=9
+    cout<< (i--) <<endl;
+    // 9 , i=8
+    cout<< (--i) <<endl;
+    // 7, i=7
+    */
+
+    /*
+    for (int a=0, b=1; a<=10 && b<=20; a++, b++) { // for ({initial value/s}; {condition to run loop}; {increment or decrement}) {}
+        cout<< a << " " << b <<endl;
+    }
+    //Break and Continue also part of FOR loop
+    */
 
 }
