812a0d584dd76794e9eec774e02b30d11bb38809
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-07 16:31:05 +0500

    bitwise operators & bit shifting

--- day-005.cpp
+++ day-005.cpp
@@ -1,1 +1,23 @@
+//FOR LOOP
+#include<iostream>
+using namespace std;
 
+int main() {
+
+    /*
+    // Bitwise Operators
+    int a = 4;
+    int b = 6;
+
+    cout<< "AND: a&b = " << (a&b) <<endl;
+    cout<< "OR: a|b = " << (a|b) <<endl;
+    cout<< "NOT: ~a = " << ~a <<endl;
+    cout<< "XOR: a^b = " << (a^b) <<endl;
+
+    // Bit Shifting
+    cout<< (17>>1) <<endl;
+    cout<< (17>>2) <<endl;
+    cout<< (19<<1) <<endl;
+    cout<< (21<<2) <<endl;
+
+}
