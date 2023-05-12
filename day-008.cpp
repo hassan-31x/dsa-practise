552c8e19c4b8ecfee210991fb8ff8c53451a52d4
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-12 15:46:39 +0500

    functions

--- day-008.cpp
+++ day-008.cpp
@@ -2,6 +2,41 @@
 #include<iostream>
 using namespace std;
 
+//Function
+int power(int a, int b) {
+    int ans = 1;
+
+    for(int i=1; i<=b; i++) {
+        ans *= a;
+    }
+
+    return ans;
+}
+
+//Function to tell if number is even or not
+bool isEven(int a) {
+    if(a&1) { //odd cuz last digit in binary is 1
+        return 0;
+    }
+    return 1;
+}
+
+//Factorial Function
+int factorial(int n) {
+    int ans=1;
+
+    // while (n>1) {
+    //     ans *= n;
+    //     n -= 1;
+    // }
+    for(int i=1; i<=n; i++) {
+        ans = ans * i;
+    }
+
+    return ans;
+}
+
+
 int main() {
 
     /*
@@ -46,5 +81,30 @@ int main() {
         default: cout<< "Please enter a valid operation" <<endl;
     }
     */
+    
+    // Function Calling
+    /*
+    int a, b;
+    cin>> a >> b;
+
+    int answer = power(a, b);
+    cout<< "Answer is: " << answer <<endl;
+    */
+
+    //nCr using Factorial Function
+    /*
+    int n, r;
+    cout<< " Enter value of n & r for calculating nCr " <<endl;
+    cin>> n >> r;
+
+
+    int nFac = factorial(n);
+    int rFac = factorial(r) * factorial(n-r);
+
+    int ans = nFac / rFac;
+    cout<< ans <<endl;
+    */
+
+    // printCounting(20);
   
 }
