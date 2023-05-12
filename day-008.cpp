1e08fa7e6480d6afab4435cc92283ec9f077e362
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-12 15:45:34 +0500

    calculator program using switch

--- day-008.cpp
+++ day-008.cpp
@@ -17,5 +17,34 @@ int main() {
         default: cout<< "It is default case" <<endl;
     }
     */
+    
+    
+    //Calculator Program
+    /*
+    int a, b;
+    char op;
+
+    cout<< " Enter the value of a " <<endl;
+    cin >> a;
+
+    cout<< " Enter the value of b " <<endl;
+    cin >> b;
+
+    cout<< " Enter the operator you want to perform " <<endl;
+    cin >> op;
+
+
+    switch( op ) {
+        case '+': cout<< a+b <<endl;
+            break;
+        case '-': cout<< a-b <<endl;
+            break;
+        case '*': cout<< a*b <<endl;
+            break;
+        case '/': cout<< a/b <<endl;
+            break;
+        default: cout<< "Please enter a valid operation" <<endl;
+    }
+    */
   
 }
