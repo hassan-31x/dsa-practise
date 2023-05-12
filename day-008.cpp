665dfd448ca560c4883d7133695af96d2a2ba223
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-12 15:44:46 +0500

    switch statements

--- day-008.cpp
+++ day-008.cpp
@@ -1,1 +1,21 @@
+//SWITCH & FUNCTIONS
+#include<iostream>
+using namespace std;
 
+int main() {
+
+    /*
+    //Switch Statements
+    int num = 2;
+    
+    cout<< endl;
+    switch( num ) {
+        case 1: cout<< "First" <<endl;
+            break;
+        case 2: cout<< "Second" <<endl;
+            break;
+        default: cout<< "It is default case" <<endl;
+    }
+    */
+  
+}
