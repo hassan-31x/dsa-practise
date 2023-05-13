4a8803a10002ccb787938a38d537a0ca0865041f
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-13 22:59:47 +0500

    printing an array

--- day-009.cpp
+++ day-009.cpp
@@ -1,1 +1,16 @@
+//ARRAYS
+#include<iostream>
+using namespace std;
 
+void printArray(int arr[], int size) {
+    for(int i=0; i<size; i++) {
+        cout<< arr[i] << " ";
+    } 
+    cout<< endl;
+}
+
+int main() {
+    int a[5] = {2, 5, 1, 9, 6};
+
+    printArray(a, 5);
+}
