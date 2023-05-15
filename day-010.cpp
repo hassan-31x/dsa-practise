44c6d429cbf797495abb2ac85f9117add1cadab1
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-15 17:17:28 +0500

    printing array

--- day-010.cpp
+++ day-010.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+using namespace std;
 
+void printArray(int arr[], int size) {
+    for(int i=0; i<size; i++) {
+        cout<< arr[i] << " ";
+    } 
+    cout<< endl;
+}
+
+int test(int arr[], int size) {
+    int ans = 0;
+
+    for(int i=0; i<size+1; i++) {
+        ans = ans^arr[i];
+        cout<<ans << " ";
+    }
+    cout<< endl;
+    for(int i=0; i<size+1; i++) {
+        ans = ans^i;
+        cout<<ans << " ";
+    }
+    cout<< endl;
+    cout<< ans;
+}
+
+
+int main() {
+    int a[11] = {2, 5, 1, -9, 6, -2, 7, 0, 21, 98, 4};
+    int b[7] = {6, 3, 1, 5, 4, 3, 2};
+    test(b, 5);
+
+    // printArray(a, 11);
+
+}
