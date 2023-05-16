e3c7b8b96bcd736768d597e3752ce5727415535a
Hassan <67674134+hassan-31x@users.noreply.github.com>
2023-05-17 00:58:33 +0500

    first & last occurances

--- day-012.cpp
+++ day-012.cpp
@@ -23,8 +23,58 @@ int binarySearch(int arr[], int size, int item) {
     return -1;
 }
 
+int firstOccurance(int arr[], int size, int item) {
+    int s = 0, e = size-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    
+    while(s<=e) {
+
+        if (arr[mid] == item) {
+            ans = mid;
+            e = mid - 1; //narrowing down the search range until it is restrcited to the first occurance of item
+        }
+        else if (item > arr[mid]) {
+            s = mid + 1;
+        }
+        else if (item < arr[mid]) {
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+int lastOccurance(int arr[], int size, int item) {
+    int s = 0, e = size-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    
+    while(s<=e) {
+
+        if (arr[mid] == item) {
+            ans = mid;
+            s = mid + 1; //narrowing down the search range until it is restrcited to the first occurance of item
+        }
+        else if (item > arr[mid]) {
+            s = mid + 1;
+        }
+        else if (item < arr[mid]) {
+            e = mid - 1;
+        }
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
 int main() {
+    int even[6] = {2, 4, 6, 8, 12, 18};
+    int odd[5] = {3, 8, 11, 14, 16};
+    int occurance[12] = {1, 2, 3, 3, 3, 3, 3, 4, 4, 4, 5, 7};
   
     // int index = binarySearch(odd, 5, 11);
   
+    // int first = firstOccurance(occurance, 12, 4);
+    // int last = lastOccurance(occurance, 12, 4);
+    // int totalOccurances = (last-first) + 1;
+    
 }
