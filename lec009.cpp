09bd277e72f8d8492210fbca24e0cff3a2b1a6cc
Hassan <muhammadhassanchannel786@gmail.com>
2023-10-16 23:58:49 +0500

    feat: arrays basics

--- lec009.cpp
+++ lec009.cpp
@@ -11,7 +11,6 @@ void printArray(int arr[], int size) {
 
 void reverse(int arr[], int size) {
 
-    // arrays have garbage random value if we don't initialise it, if we initialise with some values, only those values will be assigned, rest will be 0/false
 
     /*int temp;
     for(int i=0; i<size/2; i++) {
@@ -30,8 +29,15 @@ void reverse(int arr[], int size) {
 }
 
 int main() {
+    // arrays have garbage random value if we don't initialise it, if we initialise with some values, only those values will be assigned, rest will be 0/false
+    // array can be of any data type - char, int, float, bool, etc
     int a[5] = {2, 5, 1, 9, 6};
+
+    // we can find size of complete array using sizeof(arr)/sizeof(int) but that will tell you the size of complete array, not just till the last element
+
     reverse(a, 5);
 
     printArray(a, 5);
+
+    // arrays are passed by reference by default, so any changes made to the array inside a function will be reflected in the main function as well (it is because we pass the address of first element of array in the function)
 }
