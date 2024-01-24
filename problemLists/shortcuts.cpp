11b10fc89c87b6409f522a5857fb80d1d55fb10d
Hassan <muhammadhassanchannel786@gmail.com>
2024-01-24 18:24:10 +0500

    feat: updated desc

--- problemLists/shortcuts.cpp
+++ problemLists/shortcuts.cpp
@@ -4,6 +4,9 @@ using namespace std;
 int main() {
     //? check if a number is even or odd
     //* num&1 - returns 1 if odd and 0 if even
+
+    //? get last digit of a number
+    //* num%10 - returns last digit of a number
     
 
 }
