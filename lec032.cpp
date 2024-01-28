42501da5413ecf7d7f17e1f5148ab9c3338d8652
Hassan <muhammadhassanchannel786@gmail.com>
2024-01-28 15:42:43 +0500

    feat: recursion practise

---
+++ lec032.cpp
@@ -0,0 +1,44 @@
+//? Recursion Practice
+#include <iostream>
+using namespace std;
+
+void printFibonacci(int n) {
+    if(n == 0 || n == 1) {
+        cout << n << " ";
+        return;
+    }
+    printFibonacci(n-1);
+    printFibonacci(n-2);
+}
+
+int fibonacci(int n) {
+    if(n == 0 || n == 1) {
+        return n;
+    }
+    int prev1 = fibonacci(n-1);
+    int prev2 = fibonacci(n-2);
+    return prev1 + prev2;
+}
+
+void sayDigits(int n) {
+    string arr[10] = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+    if(n==0) {
+        return;
+    }
+
+    sayDigits(n/10);
+    cout << arr[n%10] << " ";
+}
+
+int main() {
+    int n;
+    cin >> n;
+
+    // printFibonacci(n);
+    // cout << endl;
+
+    // cout << fibonacci(n) << endl;
+
+    sayDigits(n);
+}
